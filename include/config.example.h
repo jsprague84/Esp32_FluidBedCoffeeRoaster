0a3b99ee00a7d77db5e94a7cb643cbccf1b45cf1
jsprague84 <johnathon.sprague84@gmail.com>
2025-08-30 20:07:37 -0500

    Add WiFi credentials and MQTT broker configuration

--- include/config.example.h
+++ include/config.example.h
@@ -1,8 +1,5 @@
-// filepath: //esp32CoffeeRoaster/include/config.example.h
-#ifndef CONFIG_H
-#define CONFIG_H
+
 
 const char* ssid = "your-ssid";
 const char* password = "your-password";
-
-#endif
+#define MQTT_BROKER "YOUR_MQTT_BROKER_IP_HERE"
